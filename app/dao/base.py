4aa0ed722119230c7541e35022aca42c0da9a8f4
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-19 11:45:12 +0300

    b

--- app/dao/base.py
+++ app/dao/base.py
@@ -6,7 +6,7 @@ from sqlalchemy import update as sqlalchemy_update, delete as sqlalchemy_delete,
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from bot.dao.database import Base
+from dao.database import Base
 
 # Объявляем типовой параметр T с ограничением, что это наследник Base
 T = TypeVar("T", bound=Base)
