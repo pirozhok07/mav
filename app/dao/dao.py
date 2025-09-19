1286bc2d434d0ffb28c5151e1902b8395a0f8e31
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-19 13:42:58 +0300

    fix get_stat

--- app/dao/dao.py
+++ app/dao/dao.py
@@ -57,46 +57,46 @@ class UserDAO(BaseDAO[User]):
             print(f"Ошибка при получении статистики покупок пользователя: {e}")
             return None
         
-@classmethod
-async def get_purchased_products(cls, session: AsyncSession, telegram_id: int) -> Optional[List[Purchase]]:
-    try:
-        # Запрос для получения пользователя с его покупками и связанными продуктами
-        result = await session.execute(
-            select(User)
-            .options(
-                selectinload(User.purchases).selectinload(Purchase.product)
+    @classmethod
+    async def get_purchased_products(cls, session: AsyncSession, telegram_id: int) -> Optional[List[Purchase]]:
+        try:
+            # Запрос для получения пользователя с его покупками и связанными продуктами
+            result = await session.execute(
+                select(User)
+                .options(
+                    selectinload(User.purchases).selectinload(Purchase.product)
+                )
+                .filter(User.telegram_id == telegram_id)
+                )
+            user = result.scalar_one_or_none() 
+            if user is None:
+                return None 
+            return user.purchase 
+        except SQLAlchemyError as e:
+            # Обработка ошибок при работе с базой данных
+            print(f"Ошибка при получении информации о покупках пользователя: {e}")
+            return None
+    
+    @classmethod
+    async def get_statistics(cls, session: AsyncSession):
+        try:
+            now = datetime.now()
+            query = select(
+                func.count().label('total_users'),
+                func.sum(case((cls.model.created_at >= now - timedelta(days=1), 1), else_=0)).label('new_today'),
+                func.sum(case((cls.model.created_at >= now - timedelta(days=7), 1), else_=0)).label('new_week'),
+                func.sum(case((cls.model.created_at >= now - timedelta(days=30), 1), else_=0)).label('new_month')
             )
-            .filter(User.telegram_id == telegram_id)
-            )
-        user = result.scalar_one_or_none() 
-        if user is None:
-            return None 
-        return user.purchase 
-    except SQLAlchemyError as e:
-        # Обработка ошибок при работе с базой данных
-        print(f"Ошибка при получении информации о покупках пользователя: {e}")
-        return None
-
-@classmethod
-async def get_statistics(cls, session: AsyncSession):
-    try:
-        now = datetime.now()
-        query = select(
-            func.count().label('total_users'),
-            func.sum(case((cls.model.created_at >= now - timedelta(days=1), 1), else_=0)).label('new_today'),
-            func.sum(case((cls.model.created_at >= now - timedelta(days=7), 1), else_=0)).label('new_week'),
-            func.sum(case((cls.model.created_at >= now - timedelta(days=30), 1), else_=0)).label('new_month')
-        )
-        result = await session.execute(query)
-        stats = result.fetchone()
-        statistics = {
-            'total_users': stats.total_users,
-            'new_today': stats.new_today,
-            'new_week': stats.new_week,
-            'new_month': stats.new_month
-        }
-        logger.info(f"Статистика успешно получена: {statistics}")
-        return statistics
-    except SQLAlchemyError as e:
-        logger.error(f"Ошибка при получении статистики: {e}")
-        raise
+            result = await session.execute(query)
+            stats = result.fetchone()
+            statistics = {
+                'total_users': stats.total_users,
+                'new_today': stats.new_today,
+                'new_week': stats.new_week,
+                'new_month': stats.new_month
+            }
+            logger.info(f"Статистика успешно получена: {statistics}")
+            return statistics
+        except SQLAlchemyError as e:
+            logger.error(f"Ошибка при получении статистики: {e}")
+            raise
