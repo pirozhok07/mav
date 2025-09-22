70f8475c5776c5697bf00ec4e8fb774e11bce925
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-22 11:14:58 +0300

    b

--- app/user/schemas.py
+++ app/user/schemas.py
@@ -23,6 +23,5 @@ class ProductCategoryIDModel(BaseModel):
 
 class PaymentData(BaseModel):
     user_id: int = Field(..., description="ID пользователя Telegram")
-    payment_id: str = Field(..., max_length=255, description="Уникальный ID платежа")
     price: int = Field(..., description="Сумма платежа в рублях")
     product_id: int = Field(..., description="ID товара")
