1474de54b08066729a0c5a02dda66e4c61207dcd
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-22 09:30:50 +0300

    j

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -114,7 +114,7 @@ async def admin_process_add_product(call: CallbackQuery, state: FSMContext):
 @admin_router.message(F.text, F.from_user.id.in_(settings.ADMIN_IDS), AddProduct.name)
 async def admin_process_name(message: Message, state: FSMContext):
     logger.info(f"'add descrip good'")#
-    await state.update_data(name=call.text)
+    await state.update_data(name=message.text)
     msg = await message.edit_text(text="Теперь дайте короткое описание товару: ", reply_markup=cancel_kb_inline())
     await state.update_data(last_msg_id=msg.message_id)
     await state.set_state(AddProduct.description)
