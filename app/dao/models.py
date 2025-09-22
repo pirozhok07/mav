019bf0e00b1194b56db388a24ec0a94d40031922
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-22 11:20:04 +0300

    8

--- app/dao/models.py
+++ app/dao/models.py
@@ -55,8 +55,10 @@ class Purchase(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     product_id: Mapped[int] = mapped_column(ForeignKey('products.id'))
     price: Mapped[int]
+    payment_id: Mapped[str] = mapped_column(unique=True)
     user: Mapped["User"] = relationship("User", back_populates="purchases")
     product: Mapped["Product"] = relationship("Product", back_populates="purchases")
+    status: Mapped[str] = mapped_column(Text)
 
     def __repr__(self):
         return f"<Purchase(id={self.id}, user_id={self.user_id}, product_id={self.product_id}, date={self.created_at})>"
