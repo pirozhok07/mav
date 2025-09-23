f34c7ccc5b5a4d1a985d8ad99d4256bc0a72b998
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-23 17:08:11 +0300

    c

--- app/dao/models.py
+++ app/dao/models.py
@@ -35,9 +35,10 @@ class Product(Base):
     price: Mapped[int]
     quantity: Mapped[int]
     category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+    taste_id: Mapped[int] = mapped_column(ForeignKey('tastes.id'))
     category: Mapped["Category"] = relationship("Category", back_populates="products")
-    purchases: Mapped[List['Purchase']] = relationship("Purchase", back_populates="product")
     tastes: Mapped[List['Taste'] | None] = relationship("Taste", back_populates="product")
+    purchases: Mapped[List['Purchase']] = relationship("Purchase", back_populates="product")
 
     def __repr__(self):
         return f"<Product(id={self.id}, name='{self.name}', price={self.price})>"
