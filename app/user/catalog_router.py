e49976dfe6235ea0c8c38cda1338442bd029edc8
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-25 15:44:50 +0300

    n

--- app/user/catalog_router.py
+++ app/user/catalog_router.py
@@ -26,7 +26,7 @@ async def page_catalog(call: CallbackQuery, session_without_commit: AsyncSession
 @catalog_router.callback_query(F.data.startswith("category_"))
 async def page_catalog_products(call: CallbackQuery, session_without_commit: AsyncSession):
     category_id = int(call.data.split("_")[-1])
-    product_data = await ProductDao.get_products(session=session_without_commit, telegram_id=call.from_user.id)
+    product_data = await ProductDao.get_products(session=session_without_commit, category_id=category_id)
     count_products = len(product_data)
     if count_products:
         await call.message.edit_text(
