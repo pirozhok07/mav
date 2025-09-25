bedeb68b6c0895925860e9c200168bd900794484
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-25 17:17:06 +0300

    v

--- app/user/cart_router.py
+++ app/user/cart_router.py
@@ -43,7 +43,7 @@ async def add_in_cart(call: CallbackQuery, session_with_commit: AsyncSession):
     await ProductDao.update_one_by_id(session=session_with_commit, data_id=product_id, in_cart=True)
     # await ProductDao.edit_quantity_product(session=session_with_commit, product_id=product_id, do_less=True)
     if taste_id != 0:
-        await TasteDao.edit_quantity_taste(session=session_with_commit, product_id=product_id, do_less=True)
+        await TasteDao.update_one_by_id(session=session_with_commit, data_id=product_id, in_cart=True)
     # logger.error(taste_id)
     payment_data = {
         'user_id': int(user_id),
