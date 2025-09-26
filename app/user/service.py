2f83030f1979fe38a5d373523f30d5f9d8336b60
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-26 10:17:48 +0300

    5

--- app/user/service.py
+++ app/user/service.py
@@ -1,5 +1,25 @@
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
+from aiogram.filters import Filter
+from aiogram.types import CallbackQuery
+from aiogram.fsm.context import FSMContext
 
 class NavState(StatesGroup):
     catalog = State()
+    
+class CallbackStateFilter(Filter):
+    def __init__(self, data_pattern: str = None, state:str = None):
+        self.data_pattern = data_pattern
+        self.state=state
+
+    async def __call__(self, callback: CallbackQuery, state: FSMContext) -> bool:
+        if self.data_pattern:
+            if not callback.data.startswith(self.data_pattern):
+                return False
+        if self.state:
+            current_state = await state.get_state()
+            if current_state != self.state:
+                return False
+        return True
+
+
