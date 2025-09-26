811fb3250180bb3735e3b650bef8fa965ca90af9
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-26 11:22:58 +0300

    b

--- app/user/catalog_router.py
+++ app/user/catalog_router.py
@@ -18,7 +18,7 @@ catalog_router = Router()
 
 # @catalog_router.callback_query(CallbackStateFilter(data_pattern="catalog", fstate=NavState.catalog))
 
-@catalog_router.callback_query(or_f(F.data =="catalog", StateFilter(NavState.catalog)))
+@catalog_router.callback_query(F.data =="catalog")
 async def page_catalog(call: CallbackQuery | Message, session_without_commit: AsyncSession, state: FSMContext):
     await call.answer("Загрузка каталога...")
     logger.error("True")
