10bf677fd8dd76dc2d673461dfb6546a65e2cc55
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-26 11:33:37 +0300

    5

--- app/user/cart_router.py
+++ app/user/cart_router.py
@@ -58,7 +58,7 @@ async def add_in_cart(call: CallbackQuery, session_with_commit: AsyncSession):
     # logger.error(payment_data)
     # Добавляем информацию о покупке в базу данных
     await PurchaseDao.add(session=session_with_commit, values=ItemCartData(**payment_data))
-    page_catalog(call, session_with_commit)
+    await page_catalog(call, session_with_commit)
     # product_data = await ProductDao.find_one_or_none_by_id(session=session_with_commit, data_id=int(product_id))
 
     # # Формируем уведомление администраторам
