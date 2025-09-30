af3471dde7534c6bed38c2399444d985289c758d
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-30 15:07:02 +0300

    b

--- app/dao/dao.py
+++ app/dao/dao.py
@@ -145,6 +145,7 @@ class PurchaseDao(BaseDAO[Purchase]):
                 select(Purchase)
                 .filter(Purchase.status == "NEW")
                 .order_by(Purchase.user_id)
+                .group_by(Purchase.user_id)
                 )
             total_price = result.scalars().all()
             return total_price if total_price is not None else 0
