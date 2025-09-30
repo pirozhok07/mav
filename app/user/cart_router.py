e8fa538f377e5945747898be3a9e91b51059a324
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-30 16:21:30 +0300

    v

--- app/user/cart_router.py
+++ app/user/cart_router.py
@@ -50,6 +50,7 @@ class DoOrder(StatesGroup):
 async def add_in_cart(call: CallbackQuery, session_with_commit: AsyncSession):
     await call.answer("Товар добавлен в корзину", show_alert=True)
     _, product_id, taste_id = call.data.split('_')
+    logger.error(taste_id)
     user_id = call.from_user.id
     await ProductDao.update_one_by_id(session=session_with_commit, data_id=product_id, in_cart=True)
     # await ProductDao.edit_quantity_product(session=session_with_commit, product_id=product_id, do_less=True)
