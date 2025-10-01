275821684b15e32af3c468661445647f198f1c05
pirozhok07 <vik.izrantseva@yandex.ru>
2025-10-01 16:43:08 +0300

    n

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -173,7 +173,7 @@ async def accept_order(call: CallbackQuery, session_with_commit: AsyncSession):
     purchases = await PurchaseDao.get_purchases(session=session_with_commit, telegram_id=user_id)
     for purchase in purchases:
         await PurchaseDao.change_status(session=session_with_commit, purchase_id=purchase.id, status = "CONFIRM")
-    await call.message.edit_text(text=f"{call.message}\n Подтвержден.")
+    await call.message.edit_text(text=f"{call.message.text}\n Подтвержден.")
 
 @admin_router.callback_query(F.data.startswith("delivery"), F.from_user.id.in_(settings.ADMIN_IDS))
 async def show_delivery(call: CallbackQuery, session_without_commit: AsyncSession):
