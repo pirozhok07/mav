782efe9395054dad6a125422d0bb170202e25573
pirozho07 <vik.izrantseva@yandex.ru>
2025-10-06 13:32:54 +0300

    t

--- app/migration/versions/6355985c15ad_initial_revision.py
+++ app/migration/versions/6355985c15ad_initial_revision.py
@@ -1,8 +1,8 @@
 """Initial revision
 
-Revision ID: 1a3bc5c7c739
+Revision ID: 6355985c15ad
 Revises: 
-Create Date: 2025-10-01 10:15:31.535054
+Create Date: 2025-10-06 13:31:30.087872
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '1a3bc5c7c739'
+revision: str = '6355985c15ad'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -67,7 +67,8 @@ def upgrade() -> None:
     sa.Column('taste_id', sa.Integer(), nullable=True),
     sa.Column('status', sa.Text(), nullable=False),
     sa.Column('description', sa.Text(), nullable=False),
-    sa.Column('adress', sa.Text(), nullable=False),
+    sa.Column('adress', sa.Text(), nullable=True),
+    sa.Column('date', sa.TIMESTAMP(), nullable=True),
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('created_at', sa.TIMESTAMP(), server_default=sa.text('(CURRENT_TIMESTAMP)'), nullable=False),
     sa.Column('updated_at', sa.TIMESTAMP(), server_default=sa.text('(CURRENT_TIMESTAMP)'), nullable=False),
