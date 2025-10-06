c0ef309bd502d11a0f5e611a2ce9ddbcf28970a9
pirozhok07 <vik.izrantseva@yandex.ru>
2025-10-06 15:19:12 +0300

    2

--- app/user/cart_router.py
+++ app/user/cart_router.py
@@ -177,6 +177,7 @@ async def get_adress(message: Message, state: FSMContext, session_with_commit: A
     await bot.delete_message(chat_id=message.from_user.id, message_id=message.message_id)
     await bot.delete_message(chat_id=message.from_user.id, message_id=order["last_msg_id"])
     purchases = await UserDAO.get_purchased_products(session=session_with_commit, telegram_id=message.from_user.id)
+    logger.error(order)
     for purchase in purchases:
         await PurchaseDao.set_order(session_with_commit, purchase.id, order["date"], order["adress"])
     msg = await message.answer(text="Выберите способ оплаты", reply_markup=order_kb())
