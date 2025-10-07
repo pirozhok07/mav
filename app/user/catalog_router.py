8dbb14c968ca6109e7998eaace30f1a1bf724a1d
pirozhok07 <vik.izrantseva@yandex.ru>
2025-10-07 17:01:27 +0300

    h

--- app/user/catalog_router.py
+++ app/user/catalog_router.py
@@ -11,7 +11,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from config import bot, settings
 from dao.dao import TasteDao, UserDAO, CategoryDao, ProductDao, PurchaseDao
 from user.kbs import cancele_kb, main_user_kb, catalog_kb, product_kb, get_product_buy_kb, taste_kb
-from user.schemas import TasteProductIDModel, TelegramIDModel, ProductCategoryIDModel, ItemCartData
+from user.schemas import TelegramIDModel, ProductCategoryIDModel, ItemCartData
 
 catalog_router = Router()
 
