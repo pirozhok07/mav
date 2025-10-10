22499d86c1f4936691818767d74a7bcfa02bdf66
pirozhok07 <vik.izrantseva@yandex.ru>
2025-10-10 15:48:45 +0300

    с

--- app/admin/utils.py
+++ app/admin/utils.py
@@ -1,13 +1,9 @@
-from datetime import datetime
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from loguru import logger
 
-from admin.schemas import PurchaseForDellModel
-from dao.dao import PurchaseDao
 from config import bot
-from sqlalchemy.ext.asyncio import AsyncSession
-from config import settings
+
 
 async def process_dell_text_msg(message: Message, state: FSMContext):
     data = await state.get_data()
@@ -22,25 +18,3 @@ async def process_dell_text_msg(message: Message, state: FSMContext):
 
     except Exception as e:
         logger.error(f"Произошла ошибка при удалении сообщения: {str(e)}")
-
- 
-
-async def scheduled_cleanup(session_with_commit: AsyncSession):
-    """Запланированная очистка БД"""
-    
-    for admin_id in settings.ADMIN_IDS:
-        try:
-            logger.info("🚀 Запуск ежедневной очистки БД")
-            await PurchaseDao.delete(session=session_with_commit, 
-                                     filters=PurchaseForDellModel(status="NEW"))
-            logger.info("✅ Очистка БД завершена")
-
-            # Уведомление администратору
-            await bot.send_message(
-                chat_id=admin_id,
-                text=f"✅ Ежедневная очистка БД выполнена\nВремя: {datetime.now().strftime('%Y-%m-%d %H:%M')}"
-                )
-        except Exception as e:
-            error_msg = f"❌ Ошибка при очистке БД: {e}"
-            logger.error(error_msg)
-            await bot.send_message(admin_id, error_msg)
--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
 import asyncio
 from aiogram.types import BotCommand, BotCommandScopeDefault
 from loguru import logger
-from admin.utils import scheduled_cleanup
 from config import bot, admins, dp
 from dao.database_middleware import DatabaseMiddlewareWithoutCommit, DatabaseMiddlewareWithCommit
 from admin.admin import admin_router
 from user.user_router import user_router
 from user.cart_router import cart_router
 from user.catalog_router import catalog_router
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
-
-
 
 
 
@@ -39,19 +34,6 @@ async def stop_bot():
         pass
     logger.error("Бот остановлен!")
 
-def setup_scheduler():
-    """Настройка планировщика задач"""
-    scheduler = AsyncIOScheduler()
-    
-    # Очистка каждый день в 6:00 утра
-    scheduler.add_job(
-        scheduled_cleanup,
-        trigger=CronTrigger(hour=15, minute=32),
-        id="daily_cleanup",
-        replace_existing=True
-    )
-
-
 async def main():
     # Регистрация мидлварей
     dp.update.middleware.register(DatabaseMiddlewareWithoutCommit())
@@ -67,17 +49,12 @@ async def main():
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
 
-    #Планировщик очистки
-    scheduler = setup_scheduler()
-    
     # Запуск бота в режиме long polling
     try:
         await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
     finally:
         await bot.session.close()
-        scheduler.shutdown()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
