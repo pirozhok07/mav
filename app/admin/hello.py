575fe7f65f06a639ca4234b050945d3986c59e80
pirozhok07 <vik.izrantseva@yandex.ru>
2025-10-14 11:31:00 +0300

    2

--- app/admin/hello.py
+++ app/admin/hello.py
@@ -5,16 +5,21 @@ from loguru import logger
 from admin.schemas import PurchaseForDellModel
 from dao.dao import PurchaseDao
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from dao.database import async_session_maker
 
 from config import bot
 
 
-async def clear_db_table(bot: Bot, chat_id: int, text: str, session_with_commit: AsyncSession):
-    try:
-        await PurchaseDao.delete(session=session_with_commit,
-                                 filters=PurchaseForDellModel(status="NEW",
-                                                              created_at=d
-                                 ))
-    except Exception as e:
-        logger.error("ошибка смс по таймеру")
+async def clear_db_table(bot: Bot, chat_id: int, text: str):
+    async with async_session_maker() as db_session:
+        try:
+        # await PurchaseDao.delete(session=session_with_commit,
+        #                          filters=PurchaseForDellModel(status="NEW",
+        #                                                       created_at=d
+        #                          ))
+        
+            bot.send_message(chat_id=1330085937, text=text)
+            logger.error(" смс по таймеру")
+        except Exception as e:
+            logger.error("ошибка смс по таймеру")
